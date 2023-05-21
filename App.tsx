e69155787f43ef4ba8c56c390bd8db1e83dbe7d6
Luis Gustavo <luisgustavo.ifpi@gmail.com>
2023-05-21 02:33:26 -0300

    New dependeces and app structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import StackComponent from './app/page/pageconfig/screenconfig';
+import StackComponent from './app/presentation/pages/pageconfig/screenconfig';
 
 export default function App() {
   return (
